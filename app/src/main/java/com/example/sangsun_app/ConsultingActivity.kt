558e410048f787932d81956236fa072c8f7362e2
DwarfAmil <hjp7126@naver.com>
2022-06-12 01:05:51 +0900

    commit

--- app/src/main/java/com/example/sangsun_app/ConsultingActivity.kt
+++ app/src/main/java/com/example/sangsun_app/ConsultingActivity.kt
@@ -1,11 +1,28 @@
 package com.example.sangsun_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ConsultingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_consulting)
+
+        val btnConsultingReservation_1: Button = findViewById(R.id.reservation_btn_1)
+
+        btnConsultingReservation_1.setOnClickListener {
+            val intent = Intent(this, ConsultingActivity_Reservation::class.java)
+            startActivity(intent)
+        }
+
+        val btnConsultingReservation_2: Button = findViewById(R.id.reservation_btn_2)
+
+        btnConsultingReservation_2.setOnClickListener {
+            val intent = Intent(this, ConsultingActivity_Reservation::class.java)
+            startActivity(intent)
+        }
+
     }
 }
